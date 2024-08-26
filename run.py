fec938d587d59d40af281614245e51d63e586fb9
Raykura <shoraygit@gmail.com>
2024-08-26 17:07:54 +0300

    Update run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ class WebServer():
       return "Funcionando"
 
   def run(self) -> None:
-    self.app.run(host='0.0.0.0', port=8017)
+    self.app.run(host='0.0.0.0', port=8089)
 
   def keep_alive(self):
     t = Thread(target=self.run)
