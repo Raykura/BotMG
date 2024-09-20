70eb418025bd8dc964d7d5755f4eab00b5372298
Raykura <shoraygit@gmail.com>
2024-09-20 17:59:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -154,10 +154,6 @@ if message.lower().startswith("-tipme ") and user.username== "RayMG":
                     await self.highrise.chat(f"You have been tipped {amount_str}.")
                 except (IndexError, ValueError):
                     await self.highrise.chat("Invalid tip amount. Please specify a valid number.")
-                    
-
-       
-
         if message.startswith("/fish"):
             await self.highrise.send_whisper(user.id,"fish 🎣...")
           
