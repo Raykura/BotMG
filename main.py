41c8ab818470e989786b7b4e32701c03415042ed
Raykura <shoraygit@gmail.com>
2024-09-20 17:22:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -204,11 +204,11 @@ class Bot(BaseBot):
            await self.highrise.react("heart",user.id)
            await self.highrise.send_whisper(user.id,f"@{user.username}  on the house drive safe 🍺")
 
-        if message.startswith("wine") or  message.startswith("redwine") or  message.startswith("red wine") or message.startswith("Alcohol") or  message.startswith("!beer") or message.startswith("!Drunk"):
+        if message.startswith("wine") or  message.startswith("redwine") or  message.startswith("red wine") or message.startswith("plonk") or  message.startswith("Plonk") or message.startswith("vino"):
            await self.highrise.react("heart",user.id)
-           await self.highrise.send_whisper(user.id,f"@{user.username}  on the house drive safe 🍷")
+           await self.highrise.send_whisper(user.id,f"@{user.username}  Ah, red wine—fancy!🍷 Trying to look sophisticated, or just hoping for purple teeth?🍷")
 
-       if message.startswith("water")  or  message.startswith("Water")  or  message.startswith("wet") or message.startswith("dry") or  message.startswith("Dry") or  message.startswith("glass") or message.startswith("Thirsty") or message.startswith("thirsty"):
+       if message.startswith("water") or  message.startswith("Water")  or  message.startswith("wet") or message.startswith("dry") or  message.startswith("Dry") or  message.startswith("glass") or message.startswith("Thirsty") or message.startswith("thirsty"):
            await self.highrise.react("heart",user.id)
            await self.highrise.send_whisper(user.id,f"@{user.username}  Keeping it classy with the sober vibes, I see!🚰💧")
             
