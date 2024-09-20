174f7d54c144f7d3c6ba7242c7810dcf014222db
Raykura <shoraygit@gmail.com>
2024-09-20 21:15:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1122,7 +1122,7 @@ class Bot(BaseBot):
           target_username =         message.split("@")[-1].strip()
           await                     self.teleport_to_user(user, target_username)
 
-        if        message.startswith("Summon") or       message.startswith("summon") or       message.startswith("/summom") or       message.startswith("s") or         message.startswith("S")      message.startswith("/summon") or      message.startswith("!summon"):
+        if                            message.startswith("Summon") or         message.startswith("Summom") or         message.startswith("!summom") or        message.startswith("/summom") or        message.startswith("/summon") or  message.startswith("!summon"):
           if user.username == "FallonXOXO" or user.username == "Its.Melly.Moo.XoXo" or user.username == "iced_yu" or user.username == "sh1n1gam1699" or user.username == "Abbie_38" or user.username == "hidinurbasement" or  user.username == "GothicGirlFae" or user.username == "RayMG":
            target_username = message.split("@")[-1].strip()
            await self.teleport_user_next_to(target_username, user)
