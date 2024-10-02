8d5e66755c60e85413bcd6eb412f096b949dd074
Raykura <shoraygit@gmail.com>
2024-10-02 21:38:39 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,48 +2,47 @@ from flask import Flask
 from threading import Thread
 from highrise.__main__ import *
 import time
+from asyncio import run as arun
 
+class WebServer:
+    def __init__(self):
+        self.app = Flask(__name__)
 
-class WebServer():
+        @self.app.route('/')
+        def index() -> str:
+            return "Funcionando"
 
-  def __init__(self):
-    self.app = Flask(__name__)
+    def run(self) -> None:
+        self.app.run(host='0.0.0.0', port=8084)
 
-    @self.app.route('/')
-    def index() -> str:
-      return "Funcionando"
+    def keep_alive(self):
+        t = Thread(target=self.run)
+        t.start()
 
-  def run(self) -> None:
-    self.app.run(host='0.0.0.0', port=8084)
+class RunBot:
+    room_id = "66d2726b2e80dd1f614c4dbb"  # Your room ID
+    bot_token = "202d7b11c3d00fe44e472d4c1b8ad9f4e3277e5fd03927d5cfab15532e9d8af8"  # Your bot token
+    bot_file = "main"
+    bot_class = "Bot"
 
-  def keep_alive(self):
-    t = Thread(target=self.run)
-    t.start()
+    def __init__(self) -> None:
+        self.definitions = [
+            BotDefinition(
+                getattr(import_module(self.bot_file), self.bot_class)(),
+                self.room_id, self.bot_token)
+        ]
 
-
-class RunBot():
-  room_id = "66bad059afeca0c24b497205"
-  bot_token = "60cd05e713ba823116a9e70f7fbb9418bdbca45e2c057b1fb8bf57c4c265d5f1"
-  bot_file = "main"
-  bot_class = "Bot"
-
-  def __init__(self) -> None:
-    self.definitions = [
-        BotDefinition(
-            getattr(import_module(self.bot_file), self.bot_class)(),
-            self.room_id, self.bot_token)
-    ]  # More BotDefinition classes can be added to the definitions list
-
-  def run_loop(self) -> None:
-    while True:
-      try:
-        arun(main(self.definitions))
-
-      except Exception as e:
-        print("Error: ", e)
-        time.sleep(5)
+    def run_bot(self) -> None:
+        try:
+            # Start the bot
+            arun(main(self.definitions))
+        except Exception as e:
+            print(f"Error: {e}")
+            # Prevent immediate rejoin by introducing a longer delay
+            time.sleep(30)  # Wait 30 seconds before attempting to reconnect
+            print("Attempting to reconnect...")
+            self.run_bot()  # Recursively try to run the bot again after failure
 
 if __name__ == "__main__":
-  WebServer().keep_alive()
-
-  RunBot().run_loop()
+    WebServer().keep_alive()
+    RunBot().run_bot()
