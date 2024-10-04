0eb73f9081bd1a9e600d7997fa3276aebb5e81fb
Raykura <shoraygit@gmail.com>
2024-10-04 10:39:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,29 +24,24 @@ class Bot(BaseBot):
         print("working")
         await self.highrise.walk_to(Position(3.0 , 0.25 , 1.5, "FrontRight"))
 
-   async def on_user_join(self, user: User, position: Position | AnchorPosition) -> None:
-        # Only the bot prints the message in the console
-        print(f"{user.username} (ID: {user.id})")
+async def on_user_join(self, user: User, position: Position | AnchorPosition) -> None:
+    # Only the bot prints the message in the console
+    print(f"{user.username} (ID: {user.id})")
 
-        # Announce the user has joined the room publicly
-        await self.highrise.chat(f"{user.username} joined to find a buddy!")
+    # Announce the user has joined the room publicly
+    await self.highrise.chat(f"{user.username} joined to find a buddy!")
 
-        # Send welcome whispers to the user
-        await self.highrise.send_whisper(user.id, f"❤️Welcome [{user.username}]! Use: [!emote list] or [1-97] for dances & emotes.")
-        await self.highrise.send_whisper(user.id, f"❤️Use: [/help] for more information.")
-        await self.highrise.send_whisper(user.id, f"❤Type -4 to go up 🤍.")
+    # Send welcome whispers to the user
+    await self.highrise.send_whisper(user.id, f"❤️Welcome [{user.username}]! Use: [!emote list] or [1-97] for dances & emotes.")
+    await self.highrise.send_whisper(user.id, f"❤️Use: [/help] for more information.")
+    await self.highrise.send_whisper(user.id, f"❤Type -4 to go up 🤍.")
 
-        # Send emotes
-        await self.highrise.send_emote("dance-hipshake")
-        await self.highrise.send_emote("emote-lust", user.id)
-
-        # React with a heart emoji
-        await self.highrise.react("heart", user.id)
+    # Send emotes
+    await self.highrise.send_emote("dance-hipshake")
+    await self.highrise.send_emote("emote-lust", user.id)
 
     # React with a heart emoji
     await self.highrise.react("heart", user.id)
-      
-        
     async def on_chat(self, user: User, message: str) -> None:
         print(f"{user.username}: {message}")  
 
