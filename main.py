4d8a960e6fdae7a24c099430c86b5db06f46f777
Raykura <shoraygit@gmail.com>
2024-10-04 12:22:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,8 @@ class Bot(BaseBot):
         except Exception as e:
            print(f"Error in on_chat: {e}") 
            await self.highrise.send_whisper(user.id, "An error occurred while sending your message.")
-
+                 
+        print(f"{user.username}: {message}")  
         if message.lower().startswith("!tipall ") and user.username == "RayMG":
               parts = message.split(" ")
               if len(parts) != 2:
