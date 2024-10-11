8cfe18ff9f32aa96f4123cd3d77ce70d647a78f8
Rayhebe <hebekuranyi@gmail.com>
2024-10-11 11:42:29 -0700

    Update run.py

--- run.py
+++ run.py
@@ -22,8 +22,8 @@ class WebServer():
 
 
 class RunBot():
-  room_id = "6704842a9fe8147317826caf"
-  bot_token = "1a16ce3d6ce34e8a94ba5b17e63583a4eb8f9fa6de7e024be257022d0a5fa47e"
+  room_id = "6706b1dc20084804ef575ccb"
+  bot_token = "52e5102011c184340d8a7bc34620bea190aa3fd840a62db9a7eaf58c06e636f9"
   bot_file = "main"
   bot_class = "Bot"
 
